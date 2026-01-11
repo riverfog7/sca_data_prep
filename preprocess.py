d9ea82c324360b93d5e5136704e78634e5d1b6ff
wjm <wjm9765.naver.com>
2026-01-11 21:23:52 +0900

    add folder

--- preprocess.py
+++
@@ -1,5 +0,0 @@
-from sca_data.dataset_utils import preprocess_dataset_to_24k
-from pathlib import Path
-
-DATA_DIR = Path("./Multi-stream Spontaneous Conversation Training Dataset")
-preprocess_dataset_to_24k(DATA_DIR)
